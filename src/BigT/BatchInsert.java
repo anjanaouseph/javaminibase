722f93508482d4033a784ce110b8e5ffa2b41fed
Lowkya Vuppu <vlowkya11@gmail.com>
2023-03-09 07:50:56 -0800

    Made changes to read the input from cmd and call the constructor of BatchInsert.java

--- src/BigT/BatchInsert.java
+++ src/BigT/BatchInsert.java
@@ -1,7 +1,10 @@
 package BigT;
 
 public class BatchInsert {
-    public BatchInsert(bigt table, String datafile, int type, String bigTable) {
-
+    public BatchInsert(String datafile, int type,  String bigTable, int numbuf) {
+        System.out.println("Starting to read from the data file : " + datafile);
+        System.out.println("Index type : " + type);
+        System.out.println("Table name : " + bigTable);
+        System.out.println("Number of buffers : " + numbuf);
     }
-    }
+}
