2b880a1be94ad4f41413048aaaa5107e0cac7fe7
anjana <anjana.ouseph@gmail.com>
2023-03-16 23:49:14 -0700

    my commit 2 for query

--- src/BigT/Query.java
+++ src/BigT/Query.java
@@ -4,8 +4,10 @@ import diskmgr.PCounter;
 import global.MID;
 import global.SystemDefs;
 
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,35 +15,31 @@ public class Query {
     public Query(String bigtName, int orderType, String rowFilter, String columnFilter, String valueFilter, int numBuf) {
 
         System.out.println("Index type : " + orderType);
-        System.out.println("Table name : " +bigtName );
+        System.out.println("Table name : " + bigtName);
         System.out.println("Number of buffers : " + numBuf);
-
-
-        // TODO: Check if the data file exists
-        // TODO: Check if the table exists
-        // TODO: Maybe a switch case with the index type
 
         try {
 
-            String dbpath = "/tmp/batch-insert"+System.getProperty("user.name")+".minibase-db";
-            SystemDefs sysdef = new SystemDefs( dbpath, 10000, numBuf/2, "Clock" );
-
             // Calling the constructor with the data
             bigt table = new bigt(bigtName, orderType);
-
+            // TODO ask TA : When would the buffer be freed ?
             // Reading the data inserted
             Stream stream = table.openStream(bigtName, orderType, "*", "*", "*", numBuf/4);
-            Map map = stream.getNext();
-            while (map != null) {
-//                System.out.println("---" + map.getValue());
-                map = stream.getNext();
-            }
-
-            System.out.println("READ COUNT : " + PCounter.rCounter);
-            System.out.println("WRITE COUNT : " + PCounter.wCounter);
-        } catch (Exception exception) {
-            exception.printStackTrace();
+           if(stream.getNext() != null) {
+               Map map = stream.getNext();
+               while (map != null) {
+                   System.out.println("[" + map.getRowLabel() + " " + map.getColumnLabel() + " " + map.getTimeStamp() + " " + "]"
+                           + " " + "->" + " " + map.getValue());
+                   map = stream.getNext();
+               }
+           }
+           else
+               System.out.println("Sorry Data Doesn't Exist");
         }
-
+        catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+    }
+        System.out.println("READ COUNT : " + PCounter.rCounter);
+        System.out.println("WRITE COUNT : " + PCounter.wCounter);
     }
 }
