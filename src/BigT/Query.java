751adb854ab753d974e4e9293c6c9e326c791510
anjana <anjana.ouseph@gmail.com>
2023-03-18 17:29:06 -0700

    change index type to order type

--- src/BigT/Query.java
+++ src/BigT/Query.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 public class Query {
     public Query(String bigtName, int orderType, String rowFilter, String columnFilter, String valueFilter, int numBuf) {
 
-        System.out.println("Index type : " + orderType);
+        System.out.println("Order type : " + orderType);
         System.out.println("Table name : " + bigtName);
         System.out.println("Number of buffers : " + numBuf);
 
