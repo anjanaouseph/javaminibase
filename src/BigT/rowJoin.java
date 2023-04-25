a4f8bf607d0a890293451ee6bff00d20619db8e0
nagarjun247 <48979701+nagarjun247@users.noreply.github.com>
2023-04-24 18:31:18 -0700

    type 2 join -Cartesian

--- src/BigT/rowJoin.java
+++ src/BigT/rowJoin.java
@@ -41,16 +41,11 @@ public class rowJoin {
             System.out.println("............Implementing SortMergeJoin...............");
             SortMergeJoin();
 
-        }else if(this.JoinType.equalsIgnoreCase("1")) {
+        }else{
             System.out.println("............You have chosen NestedLoopJoin...............");
             System.out.println("............Implementing NestedLoopJoin...............");
-            nestedLoopJoin();
+            cartesianJoin();
         }
-        else {
-            System.out.println("You Chosen Wrong Option. Please chose option 1 for" +
-                    "Sort Merge Join or Option 2 for Nested Loop Join");
-        }
-
     }
 
 
@@ -143,70 +138,66 @@ public class rowJoin {
                 exception.printStackTrace();
             }
         }
-        public void nestedLoopJoin() throws Exception {
+
+    public void cartesianJoin() throws Exception {
+
+        Heapfile heapfile = new Heapfile(this.outBigTName);
+        if(heapfile.getRecCntMap() != 0)//if heapfile has records in it
+        {
+            heapfile.deleteFileMap();//then delete that heapfile
+        }
+
         try {
+
             ArrayList<Map> outerRelation = new ArrayList<>();
             ArrayList<Map> innerRelation = new ArrayList<>();
 
             Map map3 = leftStream.getNext();
             while (map3 != null) {
-                Map newMap = new Map(map3);
-                outerRelation.add(newMap);
-                newMap.print();
-                map3 = leftStream.getNext();
+                Map newMap = new Map(map3); // Create a new map object with the same entries as map3
+                outerRelation.add(newMap); // Add the new map object to the list
+                map3 = leftStream.getNext(); // Get the next map from the stream
             }
             leftStream.closestream();
 
             Map map4 = rightStream.getNext();
             while (map4 != null) {
-                Map newMap = new Map(map4);
+                Map newMap = new Map(map4); // Create a new map object with the same entries as map3
                 innerRelation.add(newMap);
-                newMap.print();
                 map4 = rightStream.getNext();
             }
+
             rightStream.closestream();
 
-            if (outerRelation.get(0).getColumnLabel().isEmpty() || innerRelation.get(0).getColumnLabel().isEmpty()) {
+            if( outerRelation.isEmpty() || innerRelation.isEmpty()) {
+
+                bigt table = new bigt(this.outBigTName, 1);//create new
+            }
+            else {
+
                 bigt table = new bigt(this.outBigTName, 1);
-            } else {
-                bigt table = new bigt(this.outBigTName, 1);
+
                 for (Map outerRow : outerRelation) {
+
                     for (Map innerRow : innerRelation) {
-                        if (outerRow.getValue().equals(innerRow.getValue()) && outerRow.getTimeStamp() != innerRow.getTimeStamp()) {
-                            Map map1 = new Map();
-                            map1.setDefaultHdr();
-                            map1.setRowLabel(outerRow.getRowLabel() + ":" + innerRow.getRowLabel());
-                            map1.setColumnLabel(outerRow.getColumnLabel());
-                            map1.setTimeStamp(outerRow.getTimeStamp());
-                            map1.setValue(outerRow.getValue());
 
-                            Map map2 = new Map();
-                            map2.setDefaultHdr();
-                            map2.setRowLabel(outerRow.getRowLabel() + ":" + innerRow.getRowLabel());
-                            map2.setColumnLabel(outerRow.getColumnLabel());
-                            map2.setTimeStamp(innerRow.getTimeStamp());
-                            map2.setValue(outerRow.getValue());
+                        Map map1 = new Map();
+                        map1.setDefaultHdr();
+                        map1.setRowLabel(outerRow.getRowLabel() + ":" + innerRow.getRowLabel());
+                        map1.setColumnLabel(outerRow.getColumnLabel() + ":" + innerRow.getColumnLabel());
+                        map1.setTimeStamp(outerRow.getTimeStamp());
+                        map1.setValue(outerRow.getValue());
 
-                            MID mid1 = table.insertMap(map1, 1);
-                            MID mid2 = table.insertMap(map2, 1);
-                            table.insertIndex(mid2, map2, 0);
-                        } else if (outerRow.getValue().equals(innerRow.getValue()) && outerRow.getTimeStamp() == innerRow.getTimeStamp()) {
-                            Map map1 = new Map();
-                            map1.setDefaultHdr();
-                            map1.setRowLabel(outerRow.getRowLabel() + ":" + innerRow.getRowLabel());
-                            map1.setColumnLabel(outerRow.getColumnLabel());
-                            map1.setTimeStamp(outerRow.getTimeStamp());
-                            map1.setValue(outerRow.getValue());
+                        MID mid1 = table.insertMap(map1, 1);
+                        table.insertIndex(mid1, map1, 0);
 
-                            MID mid1 = table.insertMap(map1, 1);
-                            table.insertIndex(mid1, map1, 0);
-                        }
                     }
                 }
-                int noDuplicateRecordCount = table.deleteDuplicateRecords();
+
             }
+
         } catch (Exception exception) {
             exception.printStackTrace();
         }
     }
-    }
+}
